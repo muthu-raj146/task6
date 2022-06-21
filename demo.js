b537cfe6092a00ac2832a15d96d77154eb617833
Muthu <e.muthupandi146@gmail.com>
2022-06-21 08:53:35 +0530

    update

--- demo.js
+++ demo.js
@@ -2,14 +2,118 @@
 // <li>
 //     home
 // </li>
-let inputtext=document.getElementById("text_id");
-let items=document.getElementById("Tea_item");
+
+// /* <ul id="Tea_item">
+//                 <li>
+//                     blacktea <span class="close">&times;</span>
+//                 </li>
+//             </ul> *
+
+//     let textInput=document.getElementById("text_id");
+//     let listUl=document.getElementById("Tea_item");
+
+//     function addTea()
+//     {
+//         if(textInput.value.trim( )=="")
+//         {
+//             alert("Enter the value");
+//         }
+//         else
+//         {
+//             let liText=document.createElement("li");
+//             let liItems=document.createTextNode(textInput.value.trim());
+//             let textClose=document.createElement("span");
+
+//             liText.appendChild(liItems);
+//             listUl.appendChild(liText);
+//             textClose.classList.add("close");
+
+//             console.log();
+          
+//         }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let textInput=document.getElementById("text_id");
+let listUl=document.getElementById("Tea_item");
 
 function addTea()
 {
-let li=document.createElement("li");
-let litext=document.createTextNode(inputtext.value);
-li.appendChild(litext);
-items.prepend(li);
-console.log(li);
+
+    if(textInput.value.trim()===''){
+        alert("Enter the value..")
+    }
+    else{
+        let liText=document.createElement("li");
+        let textNode=document.createTextNode(textInput.value.trim());
+        let textClose=document.createElement("span");
+        liText.appendChild(textNode);
+        liText.appendChild(textClose);
+        listUl.appendChild(liText);
+       
+        textClose.classList.add("close");
+textClose.createclassname='close'
+        textClose.innerHTML="&times";
+        
+        textClose.addEventListener('click', (e)=>{
+            if(confirm("Are u Delete")==true){
+        console.log(e.target.parentElement);
+        let li=e.target.parentElement;
+        li.parentElement.removeChild(li);}
+        });
+        textInput.value='';
+        console.log(textInput.value.trim());
+    }
+    
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let inputtext=document.getElementById("text_id");
+// let items=document.getElementById("Tea_item");
+
+// function addTea()
+// {
+//     if(inputtext.value.trim()==("")){
+//         alert("enter the value")
+//     }
+//     else{
+//         let li=document.createElement("li");
+//         let litext=document.createTextNode(inputtext.value);
+//         li.appendChild(litext);
+//         items.prepend(li);
+//         console.log(li);
+//         inputtext.value='';
+//     }
+    
+
+// }
